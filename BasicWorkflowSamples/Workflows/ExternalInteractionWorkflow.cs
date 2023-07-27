8f755d4f17d33f68b96ea7be15bbaf1bb96db2ca
Marc Duiker <marcduiker@users.noreply.github.com>
2023-07-27 14:49:03 +0200

    Format code

--- BasicWorkflowSamples/Workflows/ExternalInteractionWorkflow.cs
+++ BasicWorkflowSamples/Workflows/ExternalInteractionWorkflow.cs
@@ -7,7 +7,9 @@ namespace BasicWorkflowSamples
         public override async Task<string> RunAsync(WorkflowContext context, string input)
         {
             var timeOut = TimeSpan.FromSeconds(20);
-            var approvalEvent = await context.WaitForExternalEventAsync<ApprovalEvent>("approval-event", timeOut);
+            var approvalEvent = await context.WaitForExternalEventAsync<ApprovalEvent>(
+                "approval-event", 
+                timeOut);
             var message = string.Empty;
 
             if (approvalEvent.IsApproved)
