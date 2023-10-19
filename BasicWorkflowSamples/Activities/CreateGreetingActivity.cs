2a8026bea16f82faa262113a9afb01cbe2d95630
Marc Duiker <marcduiker@users.noreply.github.com>
2023-10-19 09:28:42 +0200

    Add log statements
    Signed-off-by: Marc Duiker <marcduiker@users.noreply.github.com>

--- BasicWorkflowSamples/Activities/CreateGreetingActivity.cs
+++ BasicWorkflowSamples/Activities/CreateGreetingActivity.cs
@@ -6,6 +6,8 @@ namespace BasicWorkflowSamples
     {
         public override Task<string> RunAsync(WorkflowActivityContext context, string name)
         {
+            Console.WriteLine($"{nameof(CreateGreetingActivity)} with input: {name}");
+
             var greetings = new []{"Hello", "Hi", "Hey", "Hola", "Bonjour", "Ciao", "Guten Tag", "Konnichiwa"};
             var selectedGreeting = greetings[new Random().Next(0, greetings.Length)];
             var message = $"{selectedGreeting} {name}";
--- BasicWorkflowSamples/Workflows/ChainingWorkflow.cs
+++ BasicWorkflowSamples/Workflows/ChainingWorkflow.cs
@@ -1,4 +1,5 @@
 using Dapr.Workflow;
+using Google.Type;
 
 namespace BasicWorkflowSamples
 {
@@ -6,18 +7,30 @@ namespace BasicWorkflowSamples
     {
         public override async Task<string> RunAsync(WorkflowContext context, string input)
         {
+
+            // if (context.IsReplaying)
+            // {
+            //     Console.WriteLine("Replay!");
+            // } else {
+            //     Console.WriteLine("First execution!");
+            // }
+            
+            // Console.WriteLine("Activity 1");
             var message1 =  await context.CallActivityAsync<string>(
                 nameof(CreateGreetingActivity),
                 input);
             
+            // Console.WriteLine("Activity 2");
             var message2 = await context.CallActivityAsync<string>(
                 nameof(CreateGreetingActivity),
                 message1);
             
+            // Console.WriteLine("Activity 3");
             var message3 = await context.CallActivityAsync<string>(
                 nameof(CreateGreetingActivity),
                 message2);
 
+            // Console.WriteLine("Finally done!");
             return message3;
         }
     }
